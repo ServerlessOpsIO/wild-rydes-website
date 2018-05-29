f12a1d8604965f32c4f109359e0e14da65b95fd4
Tom McLaughlin <tom@serverlessops.io>
2018-05-29 10:29:38 -0400

    disable Cognito authentication

--- static/js/ride.js
+++ static/js/ride.js
@@ -5,23 +5,23 @@ WildRydes.map = WildRydes.map || {};
 
 (function rideScopeWrapper($) {
     var authToken;
-    WildRydes.authToken.then(function setAuthToken(token) {
-        if (token) {
-            authToken = token;
-        } else {
+    // Find a way to toggle this better
+    if (_config.cognito.disabled !== true) {
+        WildRydes.authToken.then(function setAuthToken(token) {
+            if (token) {
+                authToken = token;
+            } else {
+                window.location.href = '/signin.html';
+            }
+        }).catch(function handleTokenError(error) {
+            alert(error);
             window.location.href = '/signin.html';
-        }
-    }).catch(function handleTokenError(error) {
-        alert(error);
-        window.location.href = '/signin.html';
-    });
+        });
+    }
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
             url: _config.api.invokeUrl + '/ride',
-            headers: {
-                Authorization: authToken
-            },
             data: JSON.stringify({
                 PickupLocation: {
                     Latitude: pickupLocation.latitude,
