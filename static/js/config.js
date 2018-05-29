24d2b0414615e3c65d30d27527f8100cc68a29e5
Tom McLaughlin <tom@serverlessops.io>
2018-05-29 10:29:38 -0400

    disable Cognito authentication

--- static/js/config.js
+++ static/js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: '', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: '', // e.g. us-east-2
+        disabled: true
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
